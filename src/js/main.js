baabbfcb94f942c08400c3a0859cd60c28d90cb4
squizep <wonderbuf@gmail.com>
2022-07-11 16:31:35 +0300

    second commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 
+
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+	center: true,
+		loop: true,
+		margin: 30,
+		startPosition: 1,
+		items: 3,
+});
+
+$('.slider__btn--prev').click(function() {
+	// With optional speed parameter
+	// Parameters has to be in square bracket '[]'
+	owl.trigger('prev.owl.carousel');
+})
+
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
